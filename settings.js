1cdaa9efe9436de39d9cb6559a11e04b2703762b
desmondappiah222355-stack <desmondappiah222355@gmail.com>
2025-09-14 15:56:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Idk',
   author: 'Desmond',
   botName: "😇😇😇",
-  botOwner: 'Desmond(in collab with Dela', // Your name
+  botOwner: 'Desmond(in collab with Dela)', // Your name
   ownerNumber: '233249065181', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
